700c4a6f5a8b929e8f685d952563a3bc674bf4bc
Wes Gruber <116185161+wesgruberrbx@users.noreply.github.com>
2024-12-17 00:57:59 -0500

    adds lambda function

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,10 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
+import { myFirstFunction } from './my-first-function/resource';
 
 defineBackend({
-  auth,
-  data,
+    auth,
+    data,
+    myFirstFunction,
 });
