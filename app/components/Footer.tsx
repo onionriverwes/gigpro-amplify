2fcd4359ebcf3e5c4aad018b8d60c4545d90b5d6
Wes Gruber <116185161+wesgruberrbx@users.noreply.github.com>
2024-12-17 03:19:48 -0500

    updates header and footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer>
-      <p>&copy; 2024 Your Company</p>
+      <p>&copy; 2024 gigable</p>
     </footer>
   );
 }
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,15 +2,19 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li><Link href="/">Home</Link></li>
-          <li><Link href="/act-manager">Act Manager</Link></li>
-          <li><Link href="/website-manager">Website Manager</Link></li>
-          <li><Link href="/gig-manager">Gig Manager</Link></li>
-        </ul>
-      </nav>
-    </header>
+      <header>
+        <div className="logo-title">
+          <img src="/amplify.svg" alt="Logo" className="logo"/>
+          <h1 className="title">Your App Title</h1>
+        </div>
+        <nav>
+          <ul>
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/pages/act-manager">Act Manager</Link></li>
+            <li><Link href="/pages/website-manager">Website Manager</Link></li>
+            <li><Link href="/pages/gig-manager">Gig Manager</Link></li>
+          </ul>
+        </nav>
+      </header>
   );
 }
